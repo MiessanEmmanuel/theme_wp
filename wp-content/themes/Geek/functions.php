2665f82be4a3080b77181239bfa6c1ca865574d7
MiessanEmmanuel <emmanueljeanmiessan@gmail.com>
2023-05-23 17:01:20 +0000

    Template (head)

--- wp-content/themes/Geek/functions.php
+++ wp-content/themes/Geek/functions.php
@@ -1,17 +1,18 @@
 <?php
 
 //-----------------variables
-define('NAME', 'Ariel');
-
 //---------------Includes
 include(get_theme_file_path('/includes/front/enqueue.php'));
 include(get_theme_file_path('/includes/front/head.php'));
+include(get_theme_file_path('/includes/setup.php'));
 
 //---------------hooks
 
 //https://developer.wordpress.org/reference/functions/add_action/ documentation add_action
 add_action('wp_enqueue_scripts', 'g_enqueue');
 add_action('wp_head', 'g_head',5);
+add_action('after_setup_theme','g_setup_theme');
+
 
 
 
---
+++ wp-content/themes/Geek/includes/setup.php
@@ -0,0 +1,15 @@
+<?php 
+
+
+
+function g_setup_theme(){
+    add_theme_support('editor-styles');
+    add_editor_style([
+         'https://fonts.googleapis.com/css2?family=Pacifico&family=Rubik:wght@300;400;500;700&display=swap',
+        'assets/theme/bootstrap-icons/bootstrap-icons.css',
+        'assets/theme/public/index.css',
+        'assets/theme/editor.css'
+    ]
+       
+    );
+}
